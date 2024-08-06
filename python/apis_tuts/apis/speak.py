881d6c6b528975f8935eaeb329312a867f481219
kotitechy <106464507+kotitechy@users.noreply.github.com>
2024-08-06 18:11:22 +0530

    Delete python/apis_tuts/apis directory

--- python/apis_tuts/apis/speak.py
+++
@@ -1,6 +0,0 @@
-import pyttsx3 as pt
-
-engine = pt.init()
-engine.say("Hello shiva this is jarvis")
-engine.runAndWait()
-engine.stop()
--- python/apis_tuts/apis/wiki.py
+++
@@ -1,5 +0,0 @@
-import wikipedia as wk
-
-q = 'computer science'
-r = wk.summary(q, sentences=3)
-print(r)
