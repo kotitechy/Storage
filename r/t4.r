2b372a3bac97b8074c33fdee7d60dcdcce490737
kotitechy <106464507+kotitechy@users.noreply.github.com>
2024-08-07 11:29:08 +0530

    Add files via upload

---
+++ r/t4.r
@@ -0,0 +1,26 @@
+# program- 4 all dtypes
+a = 4 # numeric
+b = 3L # integer
+c = a > b  # logical
+d = 3+4i # complex
+e = "@Shiva" # character
+# check class 
+print(class(a))
+print(class(a))
+print(class(a))
+print(class(a))
+print(class(a))
+# check type of variable
+print(typeof(a))
+print(typeof(b))
+print(typeof(c))
+print(typeof(d))
+print(typeof(e))
+# check dtype of objects
+print(is.numeric(a))
+print(is.numeric(a))
+print(is.numeric(a))
+print(is.numeric(b))
+print(is.numeric(a))
+
+
