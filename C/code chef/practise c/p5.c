f112d8cd05a5a2668c1dab2819e9cf8afb34f581
kotitechy <106464507+kotitechy@users.noreply.github.com>
2024-08-13 19:18:51 +0530

    Create p5.c

---
+++ C/code chef/practise c/p5.c
@@ -0,0 +1,25 @@
+// Printing Concatenation Challenge
+
+#include <stdio.h>
+
+int main() {
+    printf("Programming"); 
+    printf("Language");
+}
+//Single printf Mastery Assessment
+
+#include <stdio.h>
+
+int main() {
+    printf("1\n2\n3");
+    return 0;
+}
+//Escape Sequence Challenge
+
+#include <stdio.h>
+
+int main() {
+    printf("This is a backslash: \\");
+    return 0;
+}
+
