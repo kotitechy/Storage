69a0518fb4102d5300f2f62d4be2b66c14c0b74e
kotitechy <106464507+kotitechy@users.noreply.github.com>
2024-08-14 11:30:18 +0530

    Create p1.r

---
+++ r/p1.r
@@ -0,0 +1,65 @@
+# Hello World
+print("Hello World")
+"Hello World"
+2
+09+123
+# variable
+name <- 'shiva'
+name
+print(name)
+
+# concatinate strings using paste function
+Country<-"India"
+paste("Hello From ",Country)
+# concatinate to variable strings
+t1 = "we are"
+t2 = "Students of VJIT"
+paste(t1,t2)
+# assigning one value to more than one variable at a time
+a<-b<-c<-"Orange"
+a
+b
+c
+# period naming convention
+.v=10
+.v
+a = TRUE
+a
+# dtypes
+a = 10 # numeric
+class(a)
+a = 10.12 # numeric
+class(a)
+
+# integer -> integer
+b = 10L
+class(b)
+typeof(b)
+# create integer 
+d = integer(23)
+class(d)
+typeof(d)
+# or
+e = as.integer(100)
+class(e)
+is.integer(e)
+# numeric-> double
+c = 10.2
+class(c)
+typeof(c)
+# character
+ch = "shiva#123"
+class(ch)
+# complex
+x = 3i
+
+
+# print details
+name<- "Shiva Chran"
+rollno <- 516
+pno <-9392953695
+branch <- 'cse-c'
+name
+rollno
+pno
+branch
