bb3b5afece9fcbf5557ae6b57af533928626c46a
kotitechy <106464507+kotitechy@users.noreply.github.com>
2024-09-17 21:45:34 +0530

    Delete practise directory

--- practise/t1.R
+++
@@ -1,4 +0,0 @@
-# program-1 - numeric, defult double
-a = 10
-print(class(a)) # numeric
-print(typeof(a)) # double
--- practise/t2.r
+++
@@ -1,6 +0,0 @@
-# program-2 integer, int dtype
-# create integer
-x = 3L
-y = as.integer(34)
-print(class(x)) # integer
-print(typeof(x)) # integer
