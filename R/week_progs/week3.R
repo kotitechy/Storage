205665d85603da4ea5598163295e05d4200fcf96
kotitechy <106464507+kotitechy@users.noreply.github.com>
2024-09-25 12:30:13 +0530

    Add files via upload
    Signed-off-by: kotitechy <106464507+kotitechy@users.noreply.github.com>

--- R/week_progs/week3.R
+++ R/week_progs/week3.R
@@ -1,7 +1,20 @@
 # r program on access and manipulate list
 
-l=list(c(1:10),list(20:30),c('omg','india'))
-l
-names(l) <- c('numbers','num','words')
-l[['num']] # 
-l[['num']][[1]][1]
+l=list(c(1:10),list(20:30),c('omg','india')) # create
+l # access
+
+l1<-(1:10)
+l1[1] # access
+l1[2]<-500  # modify
+l1[2]
+l1[-2] # delete
+
+
+
+# list-> vector
+ls<-list(1:20)
+typeof(ls)
+print(ls)
+ls1<-unlist(ls)
+typeof(ls1)
+print(ls1)
